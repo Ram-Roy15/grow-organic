a6a10d0d8f61db781b582c879dae699ed27bf120
Ram Roy <ramroycombd@gmail.com>
2024-12-20 20:24:50 +0600

    added overlayplan

--- src/components/DataTableComponent.tsx
+++ src/components/DataTableComponent.tsx
@@ -1,6 +1,9 @@
 import { useState, useEffect, useRef } from "react";
 import { DataTable } from "primereact/datatable";
 import { Column } from "primereact/column";
+
+import "primeicons/primeicons.css";
+import { FaChevronDown } from "react-icons/fa";
 import { Button } from "primereact/button";
 import { OverlayPanel } from "primereact/overlaypanel";
 import axios from "axios";
@@ -60,64 +63,124 @@ export default function CheckboxRowSelectionDemo() {
     );
   };
 
+  const op = useRef(null);
   return (
-    <div className="card">
-      <DataTable
-        value={products}
-        rows={pagination.rows}
-        paginator
-        lazy
-        totalRecords={totalRecords}
-        loading={loading}
-        onPage={onPageChange}
-        paginatorTemplate="CurrentPageReport FirstPageLink PrevPageLink PageLinks NextPageLink LastPageLink RowsPerPageDropdown"
-        currentPageReportTemplate="Showing {first} to {last} of {totalRecords}"
-        tableStyle={{ minWidth: "50rem" }}
-        selectionMode={rowClick ? undefined : "multiple"}
-        selection={selectedProducts}
-        onSelectionChange={(e) => setSelectedProducts(e.value)}
-        dataKey="id"
-      >
-        <Column
-          selectionMode="multiple"
-          headerStyle={{ width: "3rem" }}
-        ></Column>
-        <Column field="title" header="Title"></Column>
-        <Column field="place_of_origin" header="Place of Origin"></Column>
-        <Column field="artist_display" header="Artist Display"></Column>
-        <Column field="date_start" header="Date Start"></Column>
-        <Column field="date_end" header="Date End"></Column>
-        <Column
-          header="Actions"
-          body={actionBodyTemplate}
-          style={{ textAlign: "center", width: "8rem" }}
-        />
-      </DataTable>
+    <div>
+      {/* <div
+        className="overlay-panel"
+        onClick={overlayRef.current?.hide}
+        style={{
+          width: "300px",
+          height: "30px",
+          zIndex: 50,
+          position: "absolute",
 
-      <OverlayPanel ref={overlayRef} style={{ width: "300px" }}>
-        {selectedRowData ? (
-          <div>
-            <h3>{selectedRowData.title}</h3>
-            <p>
-              <strong>Artist:</strong> {selectedRowData.artist_display || "N/A"}
-            </p>
-            <p>
-              <strong>Place of Origin:</strong>{" "}
-              {selectedRowData.place_of_origin || "N/A"}
-            </p>
-            <p>
-              <strong>Inscriptions:</strong>{" "}
-              {selectedRowData.inscriptions || "N/A"}
-            </p>
-            <p>
-              <strong>Date:</strong> {selectedRowData.date_start || "N/A"} -{" "}
-              {selectedRowData.date_end || "N/A"}
-            </p>
+          transform: "translateX(10%)",
+
+          margin: "60px 0",
+        }}
+      >
+        <FaChevronDown />
+      </div> */}
+      <div className="">
+        <i
+          className="pi pi-angle-down
+"
+          style={{
+            width: "300px",
+            height: "30px",
+            zIndex: 50,
+            position: "absolute",
+
+            transform: "translateX(-185%)",
+            fontSize: "1.5rem",
+            margin: "60px 0",
+          }}
+          type="button"
+          onClick={(e) => op.current.toggle(e)}
+        ></i>
+
+        <OverlayPanel ref={op}>
+          <div className="align-items-center justify-content-center">
+            <input
+              type="number"
+              name=""
+              id=""
+              className=""
+              style={{
+                width: "200px",
+                height: "50px",
+                marginRight: "10px",
+                padding: "0 10px",
+              }}
+            />
+            <Button  label="Submit" />
           </div>
-        ) : (
-          <p>No data available</p>
-        )}
-      </OverlayPanel>
+        </OverlayPanel>
+      </div>
+      <div className="card">
+        <div>
+          <DataTable
+            value={products}
+            rows={pagination.rows}
+            paginator
+            lazy
+            totalRecords={totalRecords}
+            loading={loading}
+            onPage={onPageChange}
+            paginatorTemplate="CurrentPageReport FirstPageLink PrevPageLink PageLinks NextPageLink LastPageLink RowsPerPageDropdown"
+            currentPageReportTemplate="Showing {first} to {last} of {totalRecords}"
+            tableStyle={{ minWidth: "50rem" }}
+            selectionMode={rowClick ? undefined : "multiple"}
+            selection={selectedProducts}
+            onSelectionChange={(e) => setSelectedProducts(e.value)}
+            dataKey="id"
+          >
+            <Column
+              selectionMode="multiple"
+              headerStyle={{ width: "3rem" }}
+            ></Column>
+            <Column field="title" header="Title">
+              <FaChevronDown />
+            </Column>
+            <Column field="place_of_origin" header="Place of Origin"></Column>
+            <Column field="artist_display" header="Artist Display"></Column>
+            <Column field="date_start" header="Date Start"></Column>
+            <Column field="date_end" header="Date End"></Column>
+            <Column
+              header="Actions"
+              body={actionBodyTemplate}
+              style={{ textAlign: "center", width: "8rem" }}
+            />
+          </DataTable>
+
+          <OverlayPanel ref={overlayRef} style={{ width: "300px" }}>
+            {selectedRowData ? (
+              <div>
+                <h3>{selectedRowData.title}</h3>
+                <p>
+                  <strong>Artist:</strong>{" "}
+                  {selectedRowData.artist_display || "N/A"}
+                </p>
+                <p>
+                  <strong>Place of Origin:</strong>{" "}
+                  {selectedRowData.place_of_origin || "N/A"}
+                </p>
+                <p>
+                  <strong>Inscriptions:</strong>{" "}
+                  {selectedRowData.inscriptions || "N/A"}
+                </p>
+                <p>
+                  <strong>Date:</strong> {selectedRowData.date_start || "N/A"} -{" "}
+                  {selectedRowData.date_end || "N/A"}
+                </p>
+              </div>
+            ) : (
+              <p>No data available</p>
+            )}
+          </OverlayPanel>
+        </div>
+      </div>
     </div>
   );
 }
